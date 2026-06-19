92bc29c533801018fe20227bca79782f80bd4c43
rajveersingh11 <rajveersinghshekhawat3234@gmail.com>
2026-06-19 15:43:05 +0530

    ci: add github pages deployment workflow and configure conditional base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
+
 
